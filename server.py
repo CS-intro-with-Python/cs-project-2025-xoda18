4356ba0f177359b8b39149a8047eed8bc8aad83e
xoda18 <anknoted@gmail.com>
2025-11-09 14:45:44 +0200

    update

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/helloworld")
 def hello():
     return "Hello, world!"
+@app.route("/hello")
+def hello():
+    # 302 — обычный временный редирект
+    return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ", code=302)
