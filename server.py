4f08a5ee567aaefdad02a914696bb8558ef9d8b2
xoda18 <anknoted@gmail.com>
2025-11-09 15:13:17 +0200

    update

--- server.py
+++ server.py
@@ -2,10 +2,9 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
-@app.route("/helloworld")
-def helloworld():
-    return "Hello, world!"
-
 @app.route("/hello")
 def hello_redirect():
     return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ", code=302)
+
+if __name__ == "__main__":
+    app.run()
